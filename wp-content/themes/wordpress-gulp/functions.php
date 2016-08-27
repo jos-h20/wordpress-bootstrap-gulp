3c7454f9cae78b593258eb7fd7bed86aea593a3f
jos-h20 <josh_overly@yahoo.com>
2016-08-27 08:44:58 -0700

    add sass styling

--- wp-content/themes/wordpress-gulp/functions.php
+++ wp-content/themes/wordpress-gulp/functions.php
@@ -110,18 +110,18 @@ add_action( 'after_setup_theme', 'wordpress_gulp_content_width', 0 );
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function wordpress_gulp_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'wordpress-gulp' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'wordpress-gulp' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-}
-add_action( 'widgets_init', 'wordpress_gulp_widgets_init' );
+ function wordpress_gulp_widgets_init() {
+   register_sidebar( array(
+     'name'          => esc_html__( 'Sidebar', 'wordpress-gulp' ),
+     'id'            => 'sidebar-1',
+     'description'   => esc_html__( 'Add widgets here.', 'wordpress-gulp' ),
+     'before_widget' => '<section id="%1$s" class="panel panel-default widget %2$s">',
+     'after_widget'  => '</section>',
+     'before_title'  => '<div class="panel-heading"><h3 class="panel-title widget-title">',
+     'after_title'   => '</h3></div>',
+   ) );
+ }
+ add_action( 'widgets_init', 'wordpress_gulp_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
